c74856a8462d3b80920ff0a0ada86817b4518cff
Sasha.potapov2002 <Sasha.potapov2002@yandex.ru>
2019-11-17 19:56:49 +0300

    fixed

--- ascsite/Pages/projects/AscmdPage.cs
+++ ascsite/Pages/projects/AscmdPage.cs
@@ -83,12 +83,15 @@ namespace AscSite.Pages.projects
         public string Render()
         {
             string res = Md2Html(Body);
-            if(ProblemAuthor != null)
-                res += "<br>Author(s) of the problem: " + ProblemAuthor;
+            if (ProblemAuthor != null)
+                res += "<br>Author" + (ProblemAuthor.Contains(',') ? "s" : "") + 
+                    "s of the problem " + ProblemAuthor;
             if (InvestigationAuthor != null)
-                res += "<br>Author(s) of the investigation: " + InvestigationAuthor;
+                res += "<br>Author" + (InvestigationAuthor.Contains(',') ? "s" : "") + 
+                    "s of the investigation " + InvestigationAuthor;
             if (SolutionAuthor != null)
-                res += "<br>Author(s) of the solution: " + SolutionAuthor;
+                res += "<br>Author" + (SolutionAuthor.Contains(',') ? "s" : "") + 
+                    "s of the solution " +  SolutionAuthor;
             return res;
         }
         public string RenderAnnotation(string readMoreButton)
--- ascsite/Pages/projects/problems/exp.md
+++ ascsite/Pages/projects/problems/exp.md
@@ -8,14 +8,14 @@ Find invert function $ f(x) $ to solve $a^{x} + b^{x} = c$
 Find invert function $ f(x) $ to solve $ a^{x} + b^{x} = c^{x} $.
 
 This equation seems harder to solve, but actually it is special case of the original equation:
-$ a^{x} + b^{x} = c^{x} \Leftrightarrow ({a \over c})^{x} + ({b \over c})^{x} = 1 $, where $ c = 1 $.
+$ a^{x} + b^{x} = c^{x} \Leftrightarrow ({a \over c})^{x} + ({b \over c})^{x} = 1^{x} $.
 
 ### Investigation
 for $b = a$, this equation can be easily solved: 
 $ a^{x} + a^{x} = c \Rightarrow a^x = {c \over 2} \Rightarrow x = log_{a}({c \over 2}) $.
 
-In more general way, if we can find $ t $, such that $ b = a^{x t} $ the equation become polynomial after 
-substitution $ y = a^{x} $ and can be solve using already existing methods: $y^t + y - c = 0$.
+In more general way, if we can find $ t $, such that $ b = a^{x t} $ the equation becomes polynomial after 
+substitution $ y = a^{x} $ and can be solved using already existing methods: $y^t + y - c = 0$.
 
 @probauthor{MomoDev}
 @investauthor{MomoDev}
