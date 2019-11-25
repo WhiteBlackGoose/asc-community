58c782819a286fdbacb42fa81d659fa45a615e19
Angourisoft <31178401+Angourisoft@users.noreply.github.com>
2019-11-25 21:51:42 +0300

    Add files via upload

---
+++ CI/config.py
@@ -0,0 +1,7 @@
+class SETTINGS:
+    IPADDRESS = '128.72.116.189'
+    PORT = 23456
+    PROJECT_DIRECTORY = r'D:\main\vs_prj\asc-community\ascsite'
+    GIT_DIRECTORY = r'D:\main\vs_prj\asc-community'
+    EXECUTABLE_PATH = r'D:\main\vs_prj\asc-community\ascsite\bin\Release\netcoreapp3.0\AscSite.exe'
+    EXENAME = "AscSite.exe"
