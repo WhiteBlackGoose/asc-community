fa95bfd0775faac5f2d2a1870a851e2036df2719
Angourisoft <lev.brovchenkov@gmail.com>
2019-11-28 20:45:49 +0300

    Added latex to project page

---
+++ ascsite/Core/Interface/MathSInterface/MathSharpCompiler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace AscSite.Core.Interface
+{
+    //TODO
+    public class MathSharpCompiler
+    {
+        private StringBuilder code;
+        public MathSharpCompiler()
+        {
+            code = new StringBuilder();
+            Add("using ");
+        }
+        public void Add(string code)
+        {
+            this.code.Append(code);
+        }
+        public string Run()
+        {
+            string csProgram = code.ToString();
+            return "";
+        }
+    }
+}
