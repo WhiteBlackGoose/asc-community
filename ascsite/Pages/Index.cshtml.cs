a5f419a743cdd8f201d1c34e2df23391ef93235e
Angourisoft <lev.brovchenkov@gmail.com>
2019-12-04 22:17:46 +0300

    Links on the main page

--- ascsite/Pages/Index.cshtml.cs
+++ ascsite/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ascsite.Core;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
