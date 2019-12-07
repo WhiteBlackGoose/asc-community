7298e1fbba8638fbf16034ad71fc84a124fadd90
Angourisoft <lev.brovchenkov@gmail.com>
2019-12-07 13:10:47 +0300

    add cert load from kestrel

--- ascsite/Program.cs
+++ ascsite/Program.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 using ascsite.Core;
 using Microsoft.AspNetCore.Hosting;
@@ -18,16 +20,27 @@ namespace ascsite
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            string password = File.ReadAllText(@"D:\main\ASC-community\cert\password.txt", Encoding.UTF8);
+
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseKestrel(options =>
+                    {
+                        options.Listen(IPAddress.Loopback, 64321, listenOptions =>
+                        {
+                            listenOptions.UseHttps(@"D:\main\ASC-community\cert\cert.pfx", password);
+                        });
+                    });
                     webBuilder.UseStartup<Startup>();
                     webBuilder.UseUrls(
                         "http://*:64321",
                         "https://*:443");
-                    
+
                     webBuilder.UseWebRoot(Const.PATH_WEBROOT);
                 });
+        }
     }
 }
