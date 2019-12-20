fac0bf6e46ee84f4fef3e772783238dad825745b
Sasha.potapov2002 <Sasha.potapov2002@yandex.ru>
2019-12-20 19:11:29 +0300

    fixed css 5

--- ascsite/wwwroot/msl/msleditor.js
+++ ascsite/wwwroot/msl/msleditor.js
@@ -3,7 +3,7 @@
     cm.setValue(text);
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+function onResizeMSL() {
     var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
     if (width < 800) {
         var dw = parseFloat($(".container").css("margin-left")) +
@@ -20,6 +20,21 @@ document.addEventListener('DOMContentLoaded', function () {
         $("h6").css("padding-left", 0);
         cm.setOption("lineNumbers", false);
     }
+    else {
+        width = parseFloat($(".navbar").css("width")) - dw;
+        $(".code-editor-container").css("max-width", 1000);
+        $(".code-editor-container").css("width", width);
+        $(".code-editor-container").css("max-height", 600);
+        $(".code-editor-container").css("height", 600);
+        $(".horizontal-align-container").css("display", "table-cell");
+        $("#samples-div").insertAfter($(".code-editor-container"));
+        $("ul").css("padding-left", 15);
+        $("ul").css("max-width", width);
+        $("h6").css("padding-left", 15);
+        cm.setOption("lineNumbers", true);
+    }
     $("#msl-output").css("max-width", width);
+}
 
-});
+window.addEventListener('resize', onResizeMSL, true);
+document.addEventListener('DOMContentLoaded', onResizeMSL);
