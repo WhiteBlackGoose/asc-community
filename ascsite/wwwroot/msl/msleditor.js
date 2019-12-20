a3427c54e3bb0513a535b989a1646769ba4680af
Sasha.potapov2002 <Sasha.potapov2002@yandex.ru>
2019-12-20 18:49:29 +0300

    fixed css 4

--- ascsite/wwwroot/msl/msleditor.js
+++ ascsite/wwwroot/msl/msleditor.js
@@ -6,18 +6,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
     if (width < 800) {
-        var padding = 30;
-        width = width - padding;
+        var dw = parseFloat($(".container").css("margin-left")) +
+            parseFloat($(".container").css("padding-left")) + 50;
+        width = parseFloat($(".navbar").css("width")) - dw;
         $(".code-editor-container").css("max-width", width);
         $(".code-editor-container").css("width", width);
         $(".code-editor-container").css("max-height", width);
         $(".code-editor-container").css("height", width);
         $(".horizontal-align-container").css("display", "inline");
-        $(".huge-asc-button").css("width", width);
-        $("ul").css("padding", 0);
-        $("h6").css("padding-left", 0);
-
         $("#samples-div").insertAfter($("#msl-output"));
+        $("ul").css("padding-left", 0);
+        $("ul").css("max-width", width);
+        $("h6").css("padding-left", 0);
         cm.setOption("lineNumbers", false);
     }
     $("#msl-output").css("max-width", width);
